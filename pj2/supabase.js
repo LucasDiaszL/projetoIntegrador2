4c4c960cbabe8869cdcb3fe142d643c07bea60ec
LucasDiaszL <lucasjorge200247@gmail.com>
2025-03-26 14:59:32 -0300

    Autenticação no Logi

--- pj2/supabase.js
+++ pj2/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://your-supabase-url.supabase.co";
-const SUPABASE_ANON_KEY = "your-anon-key";
+const SUPABASE_URL = "https://lqrhuojkpuwzqgcqmawx.supabase.co";
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxxcmh1b2prcHV3enFnY3FtYXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyOTg2ODgsImV4cCI6MjA1Nzg3NDY4OH0.Y0xlKR_Uwn7RMj7hg7rbQ907Dbw4Zo1EZj8TbB43wEE";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
