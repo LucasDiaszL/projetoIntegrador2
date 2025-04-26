5187e5343177632a6c61122fe8b34ebc6f69b2f3
LucasDiaszL <lucasjorge200247@gmail.com>
2025-04-26 15:51:56 -0300

    feat: atualiza rota para página de Serviço

--- pj2/src/components/SearchDiv.jsx
+++ pj2/src/components/SearchDiv.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const SearchDiv = () => {
   return (
@@ -8,7 +10,33 @@ const SearchDiv = () => {
           className="firstDiv flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 
         shadow-lg shadow-greyIsh-700 "
         >
-          <div className="flex gap-2 items-center :"></div>
+          <div className="flex gap-2 items-center :">
+            <AiOutlineSearch className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-[var(--color-primary)] focus:outline-none w-[100%]"
+              placeholder="Pesquise o servico"
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-[var(--color-primary)] icon" />
+          </div>
+          <div className="flex gap-2 items-center :">
+            <AiOutlineSearch className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-[var(--color-primary)] focus:outline-none w-[100%]"
+              placeholder="Pesquise o servico"
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-[var(--color-primary)] icon" />
+          </div>
+          <div className="flex gap-2 items-center :">
+            <AiOutlineSearch className="text-[25px] icon" />
+            <input
+              type="text"
+              className="bg-transparent text-[var(--color-primary)] focus:outline-none w-[100%]"
+              placeholder="Pesquise o servico"
+            />
+            <AiOutlineCloseCircle className="text-[30px] text-[#a5a6a6] hover:text-[var(--color-primary)] icon" />
+          </div>
         </div>
       </form>
     </div>
--- pj2/src/pages/Servico.jsx
+++ pj2/src/pages/Servico.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import SearchDiv from "../components/SearchDiv";
 
+
 const Servico = () => {
   return(
     <>
